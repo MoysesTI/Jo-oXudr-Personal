e2e035f646fa5b8550665ef6dc51ce3645ed5fe5
MoysesTI <Moysesmv@msn.com>
2023-03-05 23:13:43 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const contact = document.querySelector('.contact')
+const menuCloser = document.querySelector('.menu-closer')
+contact.addEventListener('click', ()=>{
+    document.querySelector('.forms-container').style.display = 'flex'
+})
+
+menuCloser.addEventListener('click', ()=>{
+    document.querySelector('.forms-container').style.display = 'none'
+})
+
+const nav =  document.querySelector('.nav-a')
+const produtos = document.querySelector('.planos-sell')
+produtos.addEventListener('click', ()=>{
+    document.querySelector('.produtos').style.display = 'block'
+    document.querySelector('main').style.display = 'none'
+    
+})
+
+    window.onscroll = function() {myFunction()};
+
+    let header = document.querySelector('header');
+    var sticky = header.offsetTop;
+    let joaoXudre = document.querySelector('.joaoXudre')
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+                header.classList.add("sticky");
+        } else {
+                header.classList.remove("sticky");
+                navbars.classList.add('off_End_Onn');
+        }
+}
+const navbars = document.querySelector('.navBar');
+function menu(){
+    if(navbars.classList.contains('off_End_Onn')){
+        navbars.classList.remove('off_End_Onn');
+        header.classList.add("sticky");
+    }else{
+        header.classList.remove("sticky");
+        navbars.classList.add('off_End_Onn');
+    }
+}
+
